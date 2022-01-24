e1010d7f3e6aca5560f157d111db620ff5260896
Anish Reddy <anishisreddy95@gmail.com>
2022-01-23 21:26:58 -0500

    Use Output
    Use an Output file along with the console

--- words.py
+++ words.py
@@ -18,22 +18,34 @@ driver = webdriver.Chrome('chromedriver.exe', service_log_path = 'NUL', options=
 
 print("")
 
+out = []
+
 for word in words:
     print(word)
+    out.append(word + "\n")
     
     try:
         driver.get('https://yourdictionary.com/' + word)
         print(driver.find_element_by_class_name('pos').text)
+        out.append(driver.find_element_by_class_name('pos').text + "\n")
+
         print(driver.find_element_by_class_name('definition').text)
+        out.append(driver.find_element_by_class_name('definition').text + "\n")
 
         driver.get('https://sentence.yourdictionary.com/' + word)
-        print(driver.find_element_by_class_name('sentence-item').text)
+        out.append(driver.find_element_by_class_name('sentence-item').text + "\n")
     
     except:
         print("An error occured when getting data for this word")
+        out.append("An error occured when getting data for this word\n")
 
     
     print("")
+    out.append("/n")
 
 driver.close()
+
+f = open("output.txt", "w")
+f.writelines(out)
+f.close()
 input("Press any key to close . . .")
